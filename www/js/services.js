74d3961a074cb5836b3c4b8ce30acd2097499716
Thales Lima <thales@ciandt.com>
2016-08-02 12:20:14 -0300

    Tela de plantas

--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,45 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Plants', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var plants = [{
     id: 0,
     name: 'Manjericão',
-    lastText: 'You on your way?',
-    face: 'img/manjericc3a3o1.jpg'
-  }, {
+    description: "Minha plantinha querida",
+    img: 'img/manjericc3a3o1.jpg',
+    data: {
+      temp: 25,
+      humi: 45,
+      lumi: 40
+    }
+  }, 
+  {
     id: 1,
     name: 'Hortelã',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    description: "Minha plantinha favorita",
+    img: 'img/hortela.jpg',
+    data: {
+      temp: 25,
+      humi: 45,
+      lumi: 40
+    }
+  }
+
+  ];
 
   return {
     all: function() {
-      return chats;
+      return plants;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(plant) {
+      plants.splice(plants.indexOf(plant), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(plantId) {
+      for (var i = 0; i < plants.length; i++) {
+        if (plants[i].id === parseInt(plantId)) {
+          return plants[i];
         }
       }
       return null;
