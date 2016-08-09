98170cc26530c52160d6b85b6a46a3f7aecc0da6
Thales Lima <thales@ciandt.com>
2016-08-09 12:15:08 -0300

    Criação do modulo de API.

---
+++ www/js/api.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('starter').service('api', function($http) {
+    var url_servico = 'http://api.iot.ciandt.com/v2/data/';
+    var api={};
+
+    api.get = function(route,params) {
+        route=url_servico + route;
+        return $http.get(route,params);
+    };
+
+    api.post = function(route,params) {
+        route=url_servico + route;
+        return $http.post(route,params);
+    };
+
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('PlantsCtrl', function($scope, Plants) {
+.controller('PlantsCtrl', function($scope, api, $http) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,10 +11,24 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.plants = Plants.all();
-  $scope.remove = function(plant) {
-    Plants.remove(plant);
+  // $scope.plants = Plants.all();
+  // $scope.remove = function() {
+  //   Plants.remove(plant);
+  // };
+
+  $scope.load = function () {
+    api.get('a4:28:72:ca:55:13/last')
+    .success (function(response){
+        console.log (response);
+        $scope.plantas = response.content;
+      })
+    .error (function(err) {
+        console.log (err);
+    });
   };
+
+  $scope.plantas = [];
+  $scope.load();
 })
 
 .controller('PlantsDetailCtrl', function($scope, $stateParams, Plants) {
