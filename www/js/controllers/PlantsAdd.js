3797a42b7ddb4e7b5543126eff730302ae9d3661
Danielle Cristina Soldera <dsoldera@cit>
2017-02-15 16:21:32 -0200

    update parametros from Plants.Details to Plants.add.js

--- www/js/controllers/PlantsAdd.js
+++ www/js/controllers/PlantsAdd.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('app.controllers')
 
-    .controller('AddCtrl', function ($scope, PlantaService, $ionicLoading, $state, $stateParams, $ionicPopup, $filter) {
+    .controller('AddCtrl', function ($scope, PlantaService, PlantaParametrosService, $ionicLoading, $state, $stateParams, $ionicPopup, $filter) {
 
         $scope.$on('$ionicView.enter', function (e) {
             $scope.plant = { "ativa": true, "dataCadastro": new Date() };
@@ -77,7 +77,7 @@ angular.module('app.controllers')
                 });
         };
 
-        $scope.validarDados = function (plant) {
+        $scope.validarDados = function (plant, parametros) {
             if (typeof (plant.nome) == "undefined"||
                 (plant.nome) == "") {
                 $ionicPopup.alert({
@@ -103,9 +103,6 @@ angular.module('app.controllers')
                 return false;
             }
 
-
-            return true;
-
             // if(typeof (plant.nome) == "undefined" ||
             //   typeof (plant.descricao) == "undefined" ||
             //   typeof (plant.localizacao) == "undefined" ||
@@ -116,9 +113,107 @@ angular.module('app.controllers')
             //       template: 'Macacos me mordam! Algo saiu errado!'
             //     });
             //   }
+
+            if (typeof (parametros.humidade_ar_min) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do ar mínimo não informada.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.humidade_ar_max) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do ar máximo não informada.'
+                });
+                return false;
+            }
+
+            if (parseInt(parametros.humidade_ar_min) >= parseInt(parametros.humidade_ar_max)) {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do ar mínimo não pode ser maior ou igual a humidade do ar máximo.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.humidade_solo_min) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do solo mínimo não informada.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.humidade_solo_max) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do solo máximo não informada.'
+                });
+                return false;
+            }
+
+            if (parseInt(parametros.humidade_solo_min) >= parseInt(parametros.humidade_solo_max)) {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Humidade do solo mínimo não pode ser maior ou igual a humidade do solo máximo.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.luminosidade_min) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Luminosidade mínima não informada.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.luminosidade_max) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Luminosidade máxima não informada.'
+                });
+                return false;
+            }
+
+            if (parseInt(parametros.luminosidade_min) >= parseInt(parametros.luminosidade_max)) {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Luminosidade mínima não pode ser maior ou igual a luminosidade máxima.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.temperatura_min) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Temperatura mínima não informada.'
+                });
+                return false;
+            }
+
+            if (typeof (parametros.temperatura_max) == "undefined") {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Temperatura máxima não informada.'
+                });
+                return false;
+            }
+
+            if (parseInt(parametros.temperatura_min) >= parseInt(parametros.temperatura_max)) {
+                $ionicPopup.alert({
+                title: 'Oops',
+                template: 'Temperatura mínima não pode ser maior ou igual a temperatura máxima.'
+                });
+                return false;
+            }
+
+            return true;
         }
 
-        $scope.gravar = function (plant) {
+        $scope.gravar = function (plant, parametros) {
             //Recupera os dados do formulário digitado
             var planta = {};
             console.log(plant);
@@ -172,7 +267,59 @@ angular.module('app.controllers')
                         });
                     }
                 });
-
             }
+
+            console.log(parametros);
+            if ($scope.validarDados(parametros)) {
+                if (parametros.id != null) {
+                    parametros = {
+                        "id": parametros.id,
+                        "idPlanta": parametros.idPlanta,
+                        "humidade_ar_max": parametros.humidade_ar_max,
+                        "humidade_ar_min": parametros.humidade_ar_min,
+                        "humidade_solo_max": parametros.humidade_solo_max,
+                        "humidade_solo_min": parametros.humidade_solo_min,
+                        "luminosidade_max": parametros.luminosidade_max,
+                        "luminosidade_min": parametros.luminosidade_min,
+                        "temperatura_max": parametros.temperatura_max,
+                        "temperatura_min": parametros.temperatura_min
+                    };
+                } else {
+                    parametros= {
+                    "idPlanta": parametros.idPlanta,
+                    "humidade_ar_max": parametros.humidade_ar_max,
+                    "humidade_ar_min": parametros.humidade_ar_min,
+                    "humidade_solo_max": parametros.humidade_solo_max,
+                    "humidade_solo_min": parametros.humidade_solo_min,
+                    "luminosidade_max": parametros.luminosidade_max,
+                    "luminosidade_min": parametros.luminosidade_min,
+                    "temperatura_max": parametros.temperatura_max,
+                    "temperatura_min": parametros.temperatura_min
+                    };
+                }
+
+                console.log("Dados do formulário: ");
+                console.log(parametros);
+
+                PlantaParametrosService.addOrUpdate(parametros, function (retorno) {
+                    if (retorno.success) {
+                        var alerta = $ionicPopup.alert({
+                            title: 'Sucesso',
+                            template: 'Tudo certo! Dados gravados com sucesso!'
+                        });
+
+                        alerta.then(function (res) {
+                            $stateParams.plantId = null;
+                            $state.go('tab.plants', { reload: true });
+                        });
+                    } else {
+                        $ionicPopup.alert({
+                            title: 'Erro ao gravar os dados',
+                            template: 'Desculpe, mas ocorreu um erro ao tentar gravar os dados da planta. Mensagem: ' + retorno.erro.mensagem
+                        });
+                    }
+                });
+
+            };
         };
     });
--- www/js/controllers/PlantsDetail.js
+++ www/js/controllers/PlantsDetail.js
@@ -14,105 +14,105 @@ angular.module('app.controllers')
       }
     });
 
-    $scope.validarDados = function (parametros) {
-      if (typeof (parametros.humidade_ar_min) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do ar mínimo não informada.'
-        });
-        return false;
-      }
+    // $scope.validarDados = function (parametros) {
+    //   if (typeof (parametros.humidade_ar_min) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do ar mínimo não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.humidade_ar_max) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do ar máximo não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.humidade_ar_max) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do ar máximo não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (parseInt(parametros.humidade_ar_min) >= parseInt(parametros.humidade_ar_max)) {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do ar mínimo não pode ser maior ou igual a humidade do ar máximo.'
-        });
-        return false;
-      }
+    //   if (parseInt(parametros.humidade_ar_min) >= parseInt(parametros.humidade_ar_max)) {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do ar mínimo não pode ser maior ou igual a humidade do ar máximo.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.humidade_solo_min) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do solo mínimo não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.humidade_solo_min) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do solo mínimo não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.humidade_solo_max) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do solo máximo não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.humidade_solo_max) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do solo máximo não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (parseInt(parametros.humidade_solo_min) >= parseInt(parametros.humidade_solo_max)) {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Humidade do solo mínimo não pode ser maior ou igual a humidade do solo máximo.'
-        });
-        return false;
-      }
+    //   if (parseInt(parametros.humidade_solo_min) >= parseInt(parametros.humidade_solo_max)) {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Humidade do solo mínimo não pode ser maior ou igual a humidade do solo máximo.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.luminosidade_min) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Luminosidade mínima não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.luminosidade_min) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Luminosidade mínima não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.luminosidade_max) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Luminosidade máxima não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.luminosidade_max) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Luminosidade máxima não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (parseInt(parametros.luminosidade_min) >= parseInt(parametros.luminosidade_max)) {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Luminosidade mínima não pode ser maior ou igual a luminosidade máxima.'
-        });
-        return false;
-      }
+    //   if (parseInt(parametros.luminosidade_min) >= parseInt(parametros.luminosidade_max)) {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Luminosidade mínima não pode ser maior ou igual a luminosidade máxima.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.temperatura_min) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Temperatura mínima não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.temperatura_min) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Temperatura mínima não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (typeof (parametros.temperatura_max) == "undefined") {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Temperatura máxima não informada.'
-        });
-        return false;
-      }
+    //   if (typeof (parametros.temperatura_max) == "undefined") {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Temperatura máxima não informada.'
+    //     });
+    //     return false;
+    //   }
 
-      if (parseInt(parametros.temperatura_min) >= parseInt(parametros.temperatura_max)) {
-        $ionicPopup.alert({
-          title: 'Oops',
-          template: 'Temperatura mínima não pode ser maior ou igual a temperatura máxima.'
-        });
-        return false;
-      }
+    //   if (parseInt(parametros.temperatura_min) >= parseInt(parametros.temperatura_max)) {
+    //     $ionicPopup.alert({
+    //       title: 'Oops',
+    //       template: 'Temperatura mínima não pode ser maior ou igual a temperatura máxima.'
+    //     });
+    //     return false;
+    //   }
 
-      return true;
-    }
+    //   return true;
+    // }
 
     $scope.gravarParametros = function (parametros) {
       console.log(parametros);
@@ -121,26 +121,26 @@ angular.module('app.controllers')
             parametros = {
                 "id": parametros.id,
                 "idPlanta": parametros.idPlanta,
-                "humidade_ar_max": parametros.humidade_ar_max,
-                "humidade_ar_min": parametros.humidade_ar_min,
-                "humidade_solo_max": parametros.humidade_solo_max,
-                "humidade_solo_min": parametros.humidade_solo_min,
-                "luminosidade_max": parametros.luminosidade_max,
-                "luminosidade_min": parametros.luminosidade_min,
-                "temperatura_max": parametros.temperatura_max,
-                "temperatura_min": parametros.temperatura_min
+                // "humidade_ar_max": parametros.humidade_ar_max,
+                // "humidade_ar_min": parametros.humidade_ar_min,
+                // "humidade_solo_max": parametros.humidade_solo_max,
+                // "humidade_solo_min": parametros.humidade_solo_min,
+                // "luminosidade_max": parametros.luminosidade_max,
+                // "luminosidade_min": parametros.luminosidade_min,
+                // "temperatura_max": parametros.temperatura_max,
+                // "temperatura_min": parametros.temperatura_min
             };
         } else {
             parametros= {
               "idPlanta": parametros.idPlanta,
-              "humidade_ar_max": parametros.humidade_ar_max,
-              "humidade_ar_min": parametros.humidade_ar_min,
-              "humidade_solo_max": parametros.humidade_solo_max,
-              "humidade_solo_min": parametros.humidade_solo_min,
-              "luminosidade_max": parametros.luminosidade_max,
-              "luminosidade_min": parametros.luminosidade_min,
-              "temperatura_max": parametros.temperatura_max,
-              "temperatura_min": parametros.temperatura_min
+              // "humidade_ar_max": parametros.humidade_ar_max,
+              // "humidade_ar_min": parametros.humidade_ar_min,
+              // "humidade_solo_max": parametros.humidade_solo_max,
+              // "humidade_solo_min": parametros.humidade_solo_min,
+              // "luminosidade_max": parametros.luminosidade_max,
+              // "luminosidade_min": parametros.luminosidade_min,
+              // "temperatura_max": parametros.temperatura_max,
+              // "temperatura_min": parametros.temperatura_min
             };
         }
 
@@ -167,7 +167,5 @@ angular.module('app.controllers')
         });
 
       };
-
     };
-
   });
