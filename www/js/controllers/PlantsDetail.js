ded87f26d09fd42ee64dbf0175f97fc36d8bdddd
CIT\larissa <larissa@ciandt.com>
2017-02-24 17:05:00 -0300

    Plant details info filter. Defined plant status. Plant details status.

--- www/js/controllers/PlantsDetail.js
+++ www/js/controllers/PlantsDetail.js
@@ -3,11 +3,44 @@ angular.module('app.controllers')
 
   .controller('PlantsDetailCtrl', function ($scope, PlantaService, PlantaParametrosService, $stateParams, $ionicPopup, $state) {
 
+    $scope.setPlantStatus = function (plant) {
+      var danger = false;
+      var dados = plant.dados[0];
+      var parametros = plant.parametros;
+
+      if (dados.humidade_ar < parametros.humidade_ar_min || dados.humidade_ar > parametros.humidade_ar_max) {
+        danger = true;
+      }
+
+      if (dados.humidade_solo < parametros.humidade_solo_min || dados.humidade_solo > parametros.humidade_solo_max) {
+        danger = true;
+      }
+
+      if (dados.luminosidade < parametros.luminosidade_min || dados.luminosidade > parametros.luminosidade_max) {
+        danger = true;
+      }
+
+      if (dados.temperatura < parametros.temperatura_min || dados.temperatura > parametros.temperatura_max) {
+        danger = true;
+      }
+
+      if (danger) {
+        plant.status = "danger";
+        plant.status_message = "Socorrooooo! Preciso de ajuda!";
+        plant.icon = "icon-triste";
+      } else {
+        plant.status = "cool";
+        plant.status_message = "Yupi!!! Estou muito feliz e saudável!";
+        plant.icon = "icon-feliz";
+      }
+    }
+
     PlantaService.get($stateParams.plantId, function (retorno) {
       if (retorno.success) {
         // console.log(retorno.items.dados);
         $scope.dados = retorno.items.dados[0];
         $scope.plant = retorno.items;
+        $scope.setPlantStatus($scope.plant);
       } else {
         $ionicPopup.alert({
           title: 'Erro ao recuperar os dados',
@@ -16,45 +49,49 @@ angular.module('app.controllers')
       }
     });
 
-    $scope.editPlant = function(plant){
-      $state.go('tab.add', { plantId: plant.id });
+    $scope.editPlant = function (plant) {
+      $state.go('tab.add', {
+        plantId: plant.id
+      });
     }
 
     $scope.gravarParametros = function (parametros) {
       console.log(parametros);
       if ($scope.validarDados(parametros)) {
         if (parametros.id != null) {
-            parametros = {
-                "id": parametros.id,
-                "idPlanta": parametros.idPlanta,
+          parametros = {
+            "id": parametros.id,
+            "idPlanta": parametros.idPlanta,
 
-            };
+          };
         } else {
-            parametros= {
-              "idPlanta": parametros.idPlanta,
-            };
+          parametros = {
+            "idPlanta": parametros.idPlanta,
+          };
         }
 
         console.log("Dados do formulário: ");
         console.log(parametros);
 
         PlantaParametrosService.addOrUpdate(parametros, function (retorno) {
-            if (retorno.success) {
-                var alerta = $ionicPopup.alert({
-                    title: 'Sucesso',
-                    template: 'Tudo certo! Dados gravados com sucesso!'
-                });
+          if (retorno.success) {
+            var alerta = $ionicPopup.alert({
+              title: 'Sucesso',
+              template: 'Tudo certo! Dados gravados com sucesso!'
+            });
 
-                alerta.then(function (res) {
-                    $stateParams.plantId = null;
-                    $state.go('tab.plants', { reload: true });
-                });
-            } else {
-                $ionicPopup.alert({
-                    title: 'Erro ao gravar os dados',
-                    template: 'Desculpe, mas ocorreu um erro ao tentar gravar os dados da planta. Mensagem: ' + retorno.erro.mensagem
-                });
-            }
+            alerta.then(function (res) {
+              $stateParams.plantId = null;
+              $state.go('tab.plants', {
+                reload: true
+              });
+            });
+          } else {
+            $ionicPopup.alert({
+              title: 'Erro ao gravar os dados',
+              template: 'Desculpe, mas ocorreu um erro ao tentar gravar os dados da planta. Mensagem: ' + retorno.erro.mensagem
+            });
+          }
         });
 
       };
